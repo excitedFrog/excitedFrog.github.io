10a18b2f6afe881afc86657019ffc42ae919cd9e
JeffDev <zhefu.li@hotamil.com>
2020-04-04 14:43:32 -0500

    second chart

--- PandEconDChart/source.js
+++ PandEconDChart/source.js
@@ -1,47 +1,162 @@
+function abbrState(input, to) {
+
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['District Of Columbia', 'DC'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+
+    if (to == 'abbr') {
+        input = input.replace(/\w\S*/g, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+        for (i = 0; i < states.length; i++) {
+            if (states[i][0] == input) {
+                return (states[i][1]);
+            }
+        }
+    } else if (to == 'name') {
+        input = input.toUpperCase();
+        for (i = 0; i < states.length; i++) {
+            if (states[i][1] == input) {
+                return (states[i][0]);
+            }
+        }
+    }
+}
+
+const start_id_base = 11;
+const start_id_add = 39;
+const start_id = start_id_base + start_id_add;
+
 $.ajax({
     url: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv',
     success: function (csv) {
         csv = $.csv.toArrays(csv);
 
-        var counties = {},
+        var states = {},
+            counties = {},
             mapChart,
             countyChart,
+            stateChart,
             numRegex = /^[0-9\.]+$/,
             lastCommaRegex = /,\s$/,
             quoteRegex = /\"/g,
-            categories = csv.slice(0, 1)[0];
+            categories = csv[0];
 
         // Parse the CSV into arrays, one array each County
         $.each(csv.slice(1), function (j, row) {
-            var data = row.slice(11),
-                data2 = row.slice(11);
-            $.each(data, function (i, val) {
-                val = val.replace(quoteRegex, '');
-                if (numRegex.test(val)) {
-                    val = parseInt(val, 10);
-                } else if (!val || lastCommaRegex.test(val)) {
-                    val = null;
-                }
-                data[i] = val;
-                data2[i] = [categories[11+i], val]
-            });
+            var c_name = row[10],
+                c_fips = row[4],
+                c_data = [],
+                c_data2 = [],
+                s_name = row[6],
+                s_abbr = abbrState(s_name, 'abbr'),
+                s_data = [],
+                s_data2 = [];
 
-            counties[("00000" + row[4]).substr(-5, 5)] = {
-                name: row[10],
-                fips: row[4],
-                data: data,
-                data2: data2
-            };
+            if (s_abbr) {
+                s_abbr = s_abbr.toLowerCase();
+                $.each(row.slice(start_id), function (i, val) {
+                    val = val.replace(quoteRegex, '');
+                    if (numRegex.test(val)) {
+                        val = parseInt(val, 10);
+                    } else if (!val || lastCommaRegex.test(val)) {
+                        val = null;
+                    }
+                    c_data.push(val);
+                    c_data2.push([categories[start_id + i], val]);
+                });
+
+                counties[("00000" + row[4]).substr(-5, 5)] = {
+                    name: c_name,
+                    fips: c_fips,
+                    data: c_data,
+                    data2: c_data2,
+                    abbr: s_abbr
+                };
+
+                if (states.hasOwnProperty(s_abbr)) {
+                    var s_data_ = [],
+                        s_data2_ = [];
+                    for (var i = 0; i < c_data.length; i++) {
+                        s_data_.push(c_data[i] + states[s_abbr].data[i]);
+                        s_data2_.push([categories[start_id + i], c_data[i] + states[s_abbr].data[i]])
+                    }
+                    s_data = s_data_;
+                    s_data2 = s_data2_;
+                } else {
+                    s_data = c_data;
+                    s_data2 = c_data2;
+                }
+                states[s_abbr] = {
+                    name: s_name,
+                    abbr: s_abbr,
+                    data: s_data,
+                    data2: s_data2
+                }
+            }
+
+
         });
 
-        // For each country, use the latest value for current population
+        // For each county, use the latest value for current population
         var data = [];
         for (var fips in counties) {
             if (counties.hasOwnProperty(fips)) {
                 var value = null,
                     date_last,
                     itemData = counties[fips].data,
-                    i = itemData.length;
+                    i = itemData.length,
+                    abbr = counties[fips].abbr
 
                 while (i--) {
                     if (typeof itemData[i] === 'number') {
@@ -54,7 +169,9 @@ $.ajax({
                     name: counties[fips].name,
                     fips: fips,
                     value: value,
-                    date_last: date_last
+                    date_last: date_last,
+                    abbr: abbr,
+                    state_name: abbrState(abbr.toUpperCase(), 'name')
                 });
             }
         }
@@ -76,11 +193,11 @@ $.ajax({
                 return l.properties['hc-group'] === '__separator_lines__';
             });
 
-
         $.each(mapData, function () {
             this.id = this.properties['fips']; // for Chart.get()
-            this.flag = this.id;
+            // this.flag = this.id;
             this.name = this.name + ', ' + this.properties['hc-key'].substr(3, 2).toUpperCase();
+            this.abbr = this.properties['hc-key'].substr(3, 2);
         });
 
         // Wrap point.select to get to the total selected points
@@ -91,17 +208,17 @@ $.ajax({
             var points = mapChart.getSelectedPoints();
             if (points.length) {
                 if (points.length === 1) {
-                    $('#info #flag').attr('class', 'flag ' + points[0].flag);
                     $('#info h2').html(points[0].name);
+                    $('#info2 h2').html(points[0].state_name + ' (state-level)');
                 } else {
-                    $('#info #flag').attr('class', 'flag');
-                    $('#info h2').html('Comparing countries');
-
+                    $('#info h2').html('Comparing counties');
+                    $('#info2 h2').html('Comparing state-level');
                 }
-                $('#info .subheader').html('<h4>Number of Confirmed Case over time</h4><small><em>Shift + Click on map to compare countries</em></small>');
+                $('#info .subheader').html('<h4>Number of Confirmed Cases over time</h4><small><em>Shift + Click on map to compare counties</em></small>');
+                $('#info2 .subheader').html('<h4>Number of Confirmed Cases over time</h4>');
 
                 if (!countyChart) {
-                    countyChart = Highcharts.chart('country-chart', {
+                    countyChart = Highcharts.chart('county-chart', {
                         chart: {
                             height: 250,
                             spacingLeft: 0
@@ -136,15 +253,61 @@ $.ajax({
                                     enabled: false
                                 },
                                 threshold: 0,
-                                pointStart: parseInt(categories[11], 10)
                             }
                         }
                     });
                 }
 
+                if (!stateChart) {
+                    stateChart = Highcharts.chart('state-chart', {
+                        chart: {
+                            height: 250,
+                            spacingLeft: 0
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        title: {
+                            text: null
+                        },
+                        subtitle: {
+                            text: null
+                        },
+                        xAxis: {
+                            type: 'category',
+                            tickPixelInterval: 50,
+                            crosshair: true
+                        },
+                        yAxis: {
+                            title: null,
+                            opposite: true
+                        },
+                        tooltip: {
+                            split: true
+                        },
+                        plotOptions: {
+                            series: {
+                                animation: {
+                                    duration: 1000
+                                },
+                                marker: {
+                                    enabled: false
+                                },
+                                threshold: 0,
+                                pointStart: parseInt(categories[start_id], 10)
+                            }
+                        }
+                    });
+                }
+
+
                 countyChart.series.slice(0).forEach(function (s) {
                     s.remove(false);
                 });
+                stateChart.series.slice(0).forEach(function (s) {
+                    s.remove(false);
+                });
+
                 points.forEach(function (p) {
                     countyChart.addSeries({
                         name: p.name,
@@ -154,12 +317,24 @@ $.ajax({
                 });
                 countyChart.redraw();
 
+                points.forEach(function (p) {
+                    stateChart.addSeries({
+                        name: p.state_name,
+                        data: states[p.abbr].data2,
+                        type: points.length > 1 ? 'line' : 'area'
+                    }, false);
+                });
+                stateChart.redraw();
+
             } else {
                 $('#info #flag').attr('class', '');
                 $('#info h2').html('');
                 $('#info .subheader').html('');
                 if (countyChart) {
                     countyChart = countyChart.destroy();
+                }
+                if (stateChart) {
+                    stateChart = stateChart.destroy();
                 }
             }
         });
@@ -209,34 +384,43 @@ $.ajax({
                 footerFormat: '<span style="font-size: 10px">(Click for details)</span>'
             },
 
-            series: [{
-                data: data,
-                mapData: mapData,
-                joinBy: ['fips', 'fips'],
-                name: 'Current Case Number',
-                allowPointSelect: true,
-                borderWidth: 0.5,
-                states: {
-                    hover: {
-                        color: '#ff4d4d'
+            series: [
+                {
+                    data: data,
+                    mapData: mapData,
+                    joinBy: ['fips', 'fips'],
+                    name: 'Current Case Number',
+                    allowPointSelect: true,
+                    borderWidth: 0.5,
+                    states: {
+                        hover: {
+                            color: '#ff4d4d'
+                        },
+                        select: {
+                            color: '#b30000',
+                        }
                     },
-                    select: {
-                        color: '#b30000',
-                    }
+                    shadow: false
                 },
-                shadow: false
-            },
                 {
                     type: 'mapline',
-                    name: 'Separator',
-                    data: separatorLines,
-                    color: '#333333',
+                    name: 'State borders',
+                    data: borderLines,
+                    color: 'black',
                     shadow: false
-                }
+                },
+                // {
+                //     type: 'mapline',
+                //     name: 'Separator',
+                //     data: separatorLines,
+                //     color: 'black',
+                //     shadow: false
+                // }
             ]
         });
 
-        // Pre-select Cook County
+        // Pre-select NYC
         mapChart.get('36061').select();
+        console.log(categories)
     }
 });
